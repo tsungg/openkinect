52d13bef232ee0b5fdae63d19bb4185822655922
tsungg <tsungg@umbrellium.co.uk>
2013-10-21 17:14:16 +0100

    white space change - tabs not spaces

--- src/org/openkinect/freenect/processing/Kinect.java
+++ src/org/openkinect/freenect/processing/Kinect.java
@@ -1,28 +1,25 @@
 /**
- * ##library.name##
- * ##library.sentence##
- * ##library.url##
- *
+ * ##library.name## ##library.sentence## ##library.url##
+ * 
  * Copyright ##copyright## ##author##
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
  * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  * 
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA  02111-1307  USA
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  * 
- * @author      ##author##
- * @modified    ##date##
- * @version     ##library.prettyVersion## (##library.version##)
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * @author ##author##
+ * @modified ##date##
+ * @version ##library.prettyVersion## (##library.version##)
  */
 
 package org.openkinect.freenect.processing;
@@ -46,96 +43,97 @@ import processing.core.PImage;
  * 
  */
 public class Kinect {
-	public final static String VERSION = "##library.prettyVersion##";
+  public final static String VERSION = "##library.prettyVersion##";
 
-	private PApplet parent;
-	private Method kinectEventMethod;
-	private Context context;
-	private Device device;
+  private PApplet parent;
+  private Method kinectEventMethod;
+  private Context context;
+  private Device device;
 
-	/**
-	 * Constructor, sets the PApplet we are running inside as the parent.
-	 * 
-	 * @param parent
-	 */
-	public Kinect(PApplet parent) {
-		this.parent = parent;
-		/**
-		 * try { this.kinectEventMethod = this.parent.getClass().getMethod(
-		 * "kinectEvent", new Class[] { Kinect.class }); } catch
-		 * (NoSuchMethodException e) { throw new RuntimeException(
-		 * "You appear to be missing the kinectEvent() method.", e); } catch
-		 * (SecurityException e) { throw new RuntimeException(
-		 * "Security exception when trying to load the kinectEvent() method.",
-		 * e); }
-		 **/
-	}
+  /**
+   * Constructor, sets the PApplet we are running inside as the parent.
+   * 
+   * @param parent
+   */
+  public Kinect(PApplet parent) {
+    this.parent = parent;
+    /**
+     * try { this.kinectEventMethod = this.parent.getClass().getMethod(
+     * "kinectEvent", new Class[] { Kinect.class }); } catch
+     * (NoSuchMethodException e) { throw new RuntimeException(
+     * "You appear to be missing the kinectEvent() method.", e); } catch
+     * (SecurityException e) { throw new RuntimeException(
+     * "Security exception when trying to load the kinectEvent() method.", e); }
+     **/
+  }
 
-	/**
-	 * Start the kinect.
-	 */
-	public void start() {
-		this.start(0);
-	}
+  /**
+   * Start the kinect.
+   */
+  public void start() {
+    this.start(0);
+  }
 
-	/**
-	 * Start the kinect setting some num parameter.
-	 * 
-	 * TODO: figure out what num does in this context.
-	 * 
-	 * @param num
-	 */
-	public void start(int num) {
-		this.context = Freenect.createContext();
-		if (this.context.numDevices() < 1) {
-			System.out.println("No Kinect devices found.");
-		}
-		this.device = this.context.openDevice(num);
-		this.device.startVideo(new VideoHandler() {
-			@Override
-			public void onFrameReceived(FrameMode mode, ByteBuffer frame,
-					int timestamp) {
-				System.out.println("received video frame: " + timestamp);
-			}
-		});
+  /**
+   * Start the kinect setting some num parameter.
+   * 
+   * TODO: figure out what num does in this context.
+   * 
+   * @param num
+   */
+  public void start(int num) {
+    this.context = Freenect.createContext();
+    if (this.context.numDevices() < 1) {
+      System.out.println("No Kinect devices found.");
+    }
+    this.device = this.context.openDevice(num);
 
-		this.device.startDepth(new DepthHandler() {
+    this.device.startVideo(new VideoHandler() {
+      @Override
+      public void onFrameReceived(FrameMode mode, ByteBuffer frame,
+          int timestamp) {
+        // System.out.println("received video frame: " + timestamp);
+        System.out.println("Frame mode: " + mode.format);
+      }
+    });
 
-			@Override
-			public void onFrameReceived(FrameMode mode, ByteBuffer frame,
-					int timestamp) {
-				System.out.println("Received depth frame: " + timestamp);
-			}
-		});
-	}
+    this.device.startDepth(new DepthHandler() {
 
-	/**
-	 * Stop the Kinect.
-	 */
-	public void stop() {
-		if (this.context != null) {
-			this.context.shutdown();
-		}
-	}
+      @Override
+      public void onFrameReceived(FrameMode mode, ByteBuffer frame,
+          int timestamp) {
+        // System.out.println("Received depth frame: " + timestamp);
+      }
+    });
+  }
 
-	public void enableRGB(boolean b) {
-		this.device.setVideoFormat(VideoFormat.RGB);
-	}
+  /**
+   * Stop the Kinect.
+   */
+  public void stop() {
+    if (this.context != null) {
+      this.context.shutdown();
+    }
+  }
 
-	public PImage getVideoImage() {
-		return null;
-	}
+  public void enableRGB(boolean b) {
+    this.device.setVideoFormat(VideoFormat.RGB);
+  }
 
-	public PImage getDepthImage() {
-		return null;
-	}
+  public PImage getVideoImage() {
+    return null;
+  }
 
-	/**
-	 * return the version of the library.
-	 * 
-	 * @return String
-	 */
-	public static String version() {
-		return VERSION;
-	}
+  public PImage getDepthImage() {
+    return null;
+  }
+
+  /**
+   * return the version of the library.
+   * 
+   * @return String
+   */
+  public static String version() {
+    return VERSION;
+  }
 }
